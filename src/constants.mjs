69f279789e2eaa9658a5fc97a8721964704d812a
Magnus Lauritzen Holtet <magnulho@gmail.com>
2019-12-03 19:46:17 +0100

    Add market list, locations and sorting.

---
+++ src/constants.mjs
@@ -0,0 +1,37 @@
+export const MARKET_LIST = {
+  eiendom: 'realestate',
+  bil: 'bil',
+  torget: 'bap',
+  mc: 'mc',
+  båt: 'boat'
+};
+
+export const LOCATIONS = {
+  Akershus: 20003,
+  'Aust-Agder': 20010,
+  Buskerud: 20007,
+  Finnmark: 20020,
+  Hedmark: 20005,
+  Hordaland: 20013,
+  'Møre og Romsdal': 20015,
+  Nordland: 20018,
+  Oppland: 20006,
+  Oslo: 0.20061,
+  Rogaland: 20012,
+  'Sogn og Fjordane': 20014,
+  Telemark: 20009,
+  Troms: 20019,
+  Trøndelag: 20016,
+  Vestfold: 20008,
+  'Vest-Agder': 20011,
+  Østfold: 20002
+};
+
+export const SORT = {
+  relevant: 0,
+  nyeste: 1,
+  eldste: 2,
+  lav: 3,
+  høy: 4,
+  nærmest: 5
+};

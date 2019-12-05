4256b3e0dee88fdbd699350ba1199cf3532fd82c
Magnus Lauritzen Holtet <magnulho@gmail.com>
2019-12-05 16:52:24 +0100

    Wrap locations as strings.

--- src/constants.mjs
+++ src/constants.mjs
@@ -7,24 +7,24 @@ export const MARKET_LIST = {
 };
 
 export const LOCATIONS = {
-  Akershus: 20003,
-  'Aust-Agder': 20010,
-  Buskerud: 20007,
-  Finnmark: 20020,
-  Hedmark: 20005,
-  Hordaland: 20013,
-  'Møre og Romsdal': 20015,
-  Nordland: 20018,
-  Oppland: 20006,
-  Oslo: 0.20061,
-  Rogaland: 20012,
-  'Sogn og Fjordane': 20014,
-  Telemark: 20009,
-  Troms: 20019,
-  Trøndelag: 20016,
-  Vestfold: 20008,
-  'Vest-Agder': 20011,
-  Østfold: 20002
+  Akershus: '0.20003',
+  'Aust-Agder': '0.20010',
+  Buskerud: '0.20007',
+  Finnmark: '0.20020',
+  Hedmark: '0.20005',
+  Hordaland: '0.20013',
+  'Møre og Romsdal': '0.20015',
+  Nordland: '0.20018',
+  Oppland: '0.20006',
+  Oslo: '0.20061',
+  Rogaland: '0.20012',
+  'Sogn og Fjordane': '0.20014',
+  Telemark: '0.20009',
+  Troms: '0.20019',
+  Trøndelag: '0.20016',
+  Vestfold: '0.20008',
+  'Vest-Agder': '0.20011',
+  Østfold: '0.20002'
 };
 
 export const SORT = {
